732fa5acf4624594d4a19fad0aca4d7fbc8932a3
NS-natsu <104343731+NS-natsu@users.noreply.github.com>
2022-06-28 02:47:48 +0900

    Add files via upload

---
+++ lesson6/js/car/Interior/chair.js
@@ -0,0 +1,227 @@
+function createFrontChair(){
+	const chair = new THREE.Group();
+	
+	const angle = 64;
+	
+	//ヘッドレスト
+	let geometry = new THREE.CylinderGeometry(1, 1, 2, angle);
+	let material = new THREE.MeshLambertMaterial();
+	let mesh = new THREE.Mesh(geometry, material);
+	
+	const head = {
+		side: .7,
+		front: 1
+	}
+	
+	//形状の補正 元の形はsin(x)またはcos(x)
+	//横 sqrt(|sin(x)|)　定数倍(楕円に補正)
+	//上下 定数倍(楕円に補正)
+	let using = mesh.geometry.attributes.position.array;
+	for(let i =0; i < using.length; i+=3){/*
+		if(using[i] < 0){
+			using[i + 0] *= -1;
+			mesh.geometry.attributes.normal.array[i + 0] *= -1;
+		}
+		using[i + 0] *= size.depth;*/
+		
+		/*let tmp = Math.sin(Math.PI * using[i] / 2)
+		using[i] = Math.sqrt(Math.abs(tmp));
+		if(tmp <= 0) using[i] *= -1;*/
+		
+		tmp = using[i];
+		using[i] = Math.sqrt(Math.abs(tmp));
+		if(tmp <= 0) using[i] *= -1;
+		using[i] *= head.side;
+		
+		using[i+2] *= head.front;	
+	}
+	mesh.geometry.computeVertexNormals(); 
+	
+	mesh.position.y += 1;
+	mesh.rotation.x += Math.PI / 2;
+	mesh.scale.set(.25, .5, .25);
+	chair.add(mesh);
+	
+	//胴体部分
+	geometry = new THREE.CylinderGeometry(1, 1, 2, angle);
+	//material = new THREE.MeshNormalMaterial();
+	mesh = new THREE.Mesh(geometry, material);
+	
+	//形状の補正 元の形はsin(x)またはcos(x)
+	//前 sqrt(|sin(x)|)　定数倍(楕円に補正)
+	//後ろ sin(5πsin(x)/6)　定数倍(楕円に補正)
+	//上下 定数倍(楕円に補正)
+	using = mesh.geometry.attributes.position.array;
+	for(let i =0; i < using.length; i+=3){
+		let tmp = 0;
+		if(using[i+2] < 0){
+			tmp = Math.sqrt(-using[i+2]);
+			using[i+2] = -tmp;
+		} else {
+			tmp = Math.sin(5*Math.PI*using[i+2]/6);
+			using[i+2] = Math.sqrt(Math.abs(tmp));
+			if(tmp < 0) using[i+2] *= -1;
+		}
+		using[i+2] *= .3;
+	}
+	mesh.geometry.computeVertexNormals(); 
+	
+	mesh.rotation.x += Math.PI / 2;
+	mesh.rotation.y += 5 * Math.PI / 12;
+	//mesh.scale.set(.7, 1, 1);
+	
+	chair.add(mesh);
+	
+	//座席部分
+	geometry = new THREE.CylinderGeometry(1, 1 - .4, 1, angle);
+	//material = new THREE.MeshNormalMaterial();
+	mesh = new THREE.Mesh(geometry, material);
+	
+	//形状の補正 元の形はsin(x)またはcos(x)
+	//sqrt(|sin(πsin(x)/2)|)　定数倍(楕円に補正)
+	using = mesh.geometry.attributes.position.array;
+	for(let i =0; i < using.length; i+=3){
+		let tmp = Math.sin(Math.PI*using[i]/2);
+		using[i] = Math.sqrt(Math.abs(tmp));
+		if(tmp < 0) using[i] *= -1;
+
+		tmp = Math.sin(Math.PI*using[i+2]/2);
+		using[i+2] = Math.sqrt(Math.abs(tmp));
+		if(tmp < 0) using[i+2] *= -1;
+	}
+	mesh.geometry.computeVertexNormals(); 
+	
+	
+	mesh.position.y -= 1.2;
+	mesh.position.x -= .95;
+	//mesh.scale.set(.7, 1, 1);
+	mesh.scale.set(1.05, .6, 1.05);
+	chair.add(mesh);
+	
+	return chair;
+}
+
+function createRearChair(){
+	const chair = new THREE.Group();
+	
+	const angle = 64;
+	
+	//ヘッドレスト
+	let geometry = new THREE.CylinderGeometry(1, 1, 2, angle);
+	let material = new THREE.MeshLambertMaterial();
+	let mesh = new THREE.Mesh(geometry, material);
+	
+	const head = {
+		side: .7,
+		front: 1
+	}
+	
+	//形状の補正 元の形はsin(x)またはcos(x)
+	//横 sqrt(|sin(x)|)　定数倍(楕円に補正)
+	//上下 定数倍(楕円に補正)
+	let using = mesh.geometry.attributes.position.array;
+	for(let i =0; i < using.length; i+=3){/*
+		if(using[i] < 0){
+			using[i + 0] *= -1;
+			mesh.geometry.attributes.normal.array[i + 0] *= -1;
+		}
+		using[i + 0] *= size.depth;*/
+		
+		/*let tmp = Math.sin(Math.PI * using[i] / 2)
+		using[i] = Math.sqrt(Math.abs(tmp));
+		if(tmp <= 0) using[i] *= -1;*/
+		
+		tmp = using[i];
+		using[i] = Math.sqrt(Math.abs(tmp));
+		if(tmp <= 0) using[i] *= -1;
+		using[i] *= head.side;
+		
+		using[i+2] *= head.front;	
+	}
+	mesh.geometry.computeVertexNormals(); 
+	
+	mesh.position.y += 1;
+	mesh.position.z -= 1;
+	mesh.rotation.x += Math.PI / 2;
+	mesh.scale.set(.25, .5, .25);
+	chair.add(mesh);
+	
+	geometry = new THREE.CylinderGeometry(1, 1, 2, angle);
+	//material = new THREE.MeshNormalMaterial();
+	mesh = new THREE.Mesh(geometry, material);
+	using = mesh.geometry.attributes.position.array;
+	for(let i =0; i < using.length; i+=3){
+		
+		tmp = using[i];
+		using[i] = Math.sqrt(Math.abs(tmp));
+		if(tmp <= 0) using[i] *= -1;
+		using[i] *= head.side;
+		
+		using[i+2] *= head.front;	
+	}
+	mesh.geometry.computeVertexNormals(); 
+	
+	mesh.position.y += 1;
+	mesh.position.z += 1;
+	mesh.rotation.x += Math.PI / 2;
+	mesh.scale.set(.25, .5, .25);
+	chair.add(mesh);
+	
+	//胴体部分
+	geometry = new THREE.CylinderGeometry(1, 1, 2, angle);
+	//material = new THREE.MeshNormalMaterial();
+	mesh = new THREE.Mesh(geometry, material);
+	
+	//形状の補正 元の形はsin(x)またはcos(x)
+	//前 sqrt(|sin(x)|)　定数倍(楕円に補正)
+	//後ろ sin(5πsin(x)/6)　定数倍(楕円に補正)
+	//上下 定数倍(楕円に補正)
+	using = mesh.geometry.attributes.position.array;
+	for(let i =0; i < using.length; i+=3){
+		let tmp = 0;
+		if(using[i+2] < 0){
+			tmp = Math.sqrt(-using[i+2]);
+			using[i+2] = -tmp;
+		} else {
+			tmp = Math.sin(5*Math.PI*using[i+2]/6);
+			using[i+2] = Math.sqrt(Math.abs(tmp));
+			if(tmp < 0) using[i+2] *= -1;
+		}
+		using[i+2] *= .3;
+	}
+	mesh.geometry.computeVertexNormals(); 
+	
+	mesh.rotation.x += Math.PI / 2;
+	mesh.rotation.y += 5 * Math.PI / 12;
+	mesh.scale.set(1, 2, 1);
+	
+	chair.add(mesh);
+	
+	//座席部分
+	geometry = new THREE.CylinderGeometry(1, 1 - .4, 1, angle);
+	//material = new THREE.MeshNormalMaterial();
+	mesh = new THREE.Mesh(geometry, material);
+	
+	//形状の補正 元の形はsin(x)またはcos(x)
+	//sqrt(|sin(πsin(x)/2)|)　定数倍(楕円に補正)
+	using = mesh.geometry.attributes.position.array;
+	for(let i =0; i < using.length; i+=3){
+		let tmp = Math.sin(Math.PI*using[i]/2);
+		using[i] = Math.sqrt(Math.abs(tmp));
+		if(tmp < 0) using[i] *= -1;
+
+		tmp = Math.sin(Math.PI*using[i+2]/2);
+		using[i+2] = Math.sqrt(Math.abs(tmp));
+		if(tmp < 0) using[i+2] *= -1;
+	}
+	mesh.geometry.computeVertexNormals(); 
+	
+	
+	mesh.position.y -= 1.2;
+	mesh.position.x -= .95;
+	//mesh.scale.set(.7, 1, 1);
+	mesh.scale.set(1.05, .6, 1.05 * 2);
+	chair.add(mesh);
+	
+	return chair;
+}
---
+++ lesson6/js/car/Interior/interior.js
@@ -0,0 +1,67 @@
+function createInterior(vehicle, body){
+	const textureLoader = new THREE.TextureLoader();
+	
+	const mats = [
+		new THREE.MeshBasicMaterial({color: 0xdddddd, side: THREE.BackSide}),
+		new THREE.MeshBasicMaterial({color: 0xdddddd, side: THREE.BackSide}),
+		new THREE.MeshBasicMaterial({color: 0xdddddd, side: THREE.BackSide}),
+		new THREE.MeshLambertMaterial({map: textureLoader.load("img/image146.jpg"), side: THREE.BackSide}),
+		new THREE.MeshBasicMaterial({color: 0xdddddd, side: THREE.BackSide}),
+		new THREE.MeshBasicMaterial({color: 0xdddddd, side: THREE.BackSide})
+	];
+	
+	//mats[3].bumpMap = mats[3].map;
+	//mats[3].bumpscale = .2;
+	
+	const inter = new THREE.Mesh(
+			new THREE.BoxGeometry(vehicle.l - vehicle.f - .001, vehicle.h * vehicle.u - 0.2, vehicle.w - .01),
+			new THREE.MeshFaceMaterial(mats)
+	);
+	const vert = inter.geometry.attributes.position.array;
+	const normal = inter.geometry.attributes.normal.array;
+	
+	
+	for(let i = 0; i < 12; i++){
+		vert[24 + i] = body.position.array[24 + i];
+		normal[24 + i] = body.normal.array[24 + i];
+	}
+
+	vert[6 + 1] += .35;
+	vert[9 + 1] += .35;
+	
+	vert[24 + 1] += vehicle.h * vehicle.t + .1;
+	//vert[27 + 0] -= .0;
+	vert[27 + 1] += vehicle.h * vehicle.t + .1;
+	vert[30 + 1] += vehicle.h * vehicle.t + .1;
+	//vert[33 + 0] -= .0;
+	vert[33 + 1] += vehicle.h * vehicle.t + .1;
+	
+	vert[36 + 0] += 1.025;
+	vert[39 + 0] -= .25;
+	vert[42 + 0] += 1.025;
+	vert[45 + 0] -= .25;
+
+	inter.position.x -= vehicle.f / 2;
+	inter.position.y += .1;
+	
+	return inter;
+}
+
+function createHandle(){
+	const material = new THREE.MeshNormalMaterial();
+	const handle = new THREE.Group();
+	
+	let mesh = new THREE.Mesh(
+		new THREE.TorusGeometry(2, .25, 32, 32),
+		material
+	);
+	/*let using = mesh.geometry.attributes.position.array;
+	for(let i = 0; i < using.length; i+=3){
+		using[i + 1] = Math.abs(using[i + 1]);
+	}*/
+	//mesh.geometry.computeVertexNormals(); 
+	
+	handle.add(mesh);
+	
+	return handle;
+}
